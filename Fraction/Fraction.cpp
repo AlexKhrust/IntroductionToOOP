1a85e87c37b58f3bce68365905d7a5d4578fe079
Хрусталёв Александр <Tikkirey90_90@mail.ru>
2022-11-26 09:12:53 +0300

    homework: operator overload

--- Fraction/Fraction.cpp
+++ Fraction/Fraction.cpp
@@ -104,11 +104,139 @@ public:
         else if (integer == 0) cout << 0;
         cout << endl;
     }
+
+    void reduce()   //Сокращение дробей
+    {
+        int k = 0;
+        
+        for (int i = 999; i > 1; i--)
+        {
+            if (numerator % i == 0 && denominator % i == 0)
+            {
+                numerator /= i;
+                denominator /= i;
+                k++;
+            }
+        }
+        if (k == 0)
+        {
+            cout << "Дробь не сокращаемая." << endl;
+        }
+        else
+        {
+            cout << "Дробь после сокращения: "; 
+            print();
+        }
+
+    }
+    //перегрузка арифметических операторов:
+    Fraction operator + (Fraction &T) {return Fraction (this->integer + T.integer, this->numerator + T.numerator, this->denominator + T.denominator);}
+    Fraction operator - (Fraction &T) {return Fraction (this->integer - T.integer, this->numerator - T.numerator, this->denominator - T.denominator);}
+    Fraction operator * (Fraction &T) {return Fraction (this->integer * T.integer, this->numerator * T.numerator, this->denominator * T.denominator);}
+    Fraction operator / (Fraction &T) {return Fraction (this->integer / T.integer, this->numerator / T.numerator, this->denominator / T.denominator);}
+
+    //перегрузка операторов инкремент/декремент
+    Fraction& operator ++ () //префиксный
+    {
+        ++integer, ++numerator, ++denominator;
+        return *this;
+    } 
+    Fraction& operator -- () //префиксный
+    {
+        --integer, --numerator, --denominator;
+        return *this;
+    }
+    
+    /* ПОСТФИКСНЫЙ НЕ РАБОТАЕТ (я не понял как его сделать для объекта с несколькими полями)
+    Fraction operator ++ (int) //постфиксный
+    {
+        integer++, numerator++, denominator++;
+        return *this;
+    }
+     Fraction operator -- (int) //постфиксный
+    {
+        integer --, numerator--, denominator--;
+        return *this;
+    }*/
+
+    //перегрузка оператора присваивания
+    Fraction& operator = (const Fraction& fraction)
+    {
+        integer = fraction.integer;
+        numerator = fraction.numerator;
+        denominator = fraction.denominator;
+        return *this;
+    }
+    
+    //перегрузка операторов составного присваивания
+    Fraction& operator += (Fraction& fraction)
+    {
+        integer += fraction.integer;
+        numerator += fraction.numerator;
+        denominator += fraction.denominator;
+        return *this;
+    }
+    
+    Fraction& operator -= (Fraction& fraction)
+    {
+        integer -= fraction.integer;
+        numerator -= fraction.numerator;
+        denominator -= fraction.denominator;
+        return *this;
+    }
+
+    Fraction& operator *= (Fraction& fraction)
+    {
+        integer *= fraction.integer;
+        numerator *= fraction.numerator;
+        denominator *= fraction.denominator;
+        return *this;
+    }
+
+    Fraction& operator /= (Fraction& fraction)
+    {
+        integer /= fraction.integer;
+        numerator /= fraction.numerator;
+        denominator /= fraction.denominator;
+        return *this;
+    }
+
+    //перегрузка операторов сравнения
+    bool operator == (const Fraction& fraction)
+    {
+        return (this->integer == fraction.integer && this->numerator == fraction.numerator && this->denominator == fraction.denominator);
+    }
+
+    bool operator != (const Fraction& fraction)
+    {
+        return (this->integer != fraction.integer || this->numerator != fraction.numerator || this->denominator != fraction.denominator);
+    }
+
+    bool operator > (const Fraction& fraction)
+    {
+        return (this->integer > fraction.integer && this->numerator > fraction.numerator && this->denominator > fraction.denominator);
+    }
+
+    bool operator < (const Fraction& fraction)
+    {
+        return (this->integer < fraction.integer && this->numerator < fraction.numerator && this->denominator < fraction.denominator);
+    }
+
+    bool operator >= (const Fraction& fraction)
+    {
+        return (this->integer >= fraction.integer && this->numerator >= fraction.numerator && this->denominator >= fraction.denominator);
+    }
+
+    bool operator <= (const Fraction& fraction)
+    {
+        return (this->integer <= fraction.integer && this->numerator <= fraction.numerator && this->denominator <= fraction.denominator);
+    }
+
 };
 
 //#define CONSTRUCTORS_CHECK
 
-void main()
+int main()
 {
     setlocale(LC_ALL, "");
 
@@ -135,8 +263,32 @@ void main()
 
     Fraction A(2, 3, 4);
     Fraction B(3, 4, 5);
-    Fraction C = A * B;
+    Fraction D(2, 1, 2);
+    Fraction C = A + B;
+    Fraction S = A * D;
+    cout << "Object C = " << "\t";
     C.print();
+    cout << "Object S = " << "\t";
+    S.print();
+    cout << "Object D = " << "\t";
+    D.print();
+    cout << "Object C = " << "\t";
+    --C;
+    C.print();
+    cout << "Object C = " << "\t";
+    C = S;
+    C.print();
+    cout << "Object D = " << "\t";
+    D += S;
+    D.print();
 
+    if (C == S) cout<<"yes"<<endl;
+    if (D != S) cout << "yes" << endl;
+    if (D > S) cout << "yes" << endl;
+    if (C <= S) cout << "yes" << endl;
 
+    Fraction F(5, 26, 36);  //Проверочный код для метода "void reduce()"
+    F.print();
+    F.reduce();
+    F.print();
 }
